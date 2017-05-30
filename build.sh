ac99f3899e825026b44a2b1ca0825a7e7cce038d
Sam McMahon <sammcmahon@u.boisestate.edu>
2017-05-30 17:02:35 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,6 +13,7 @@ git clone https://github.com/scrooloose/nerdtree.git
 git clone https://github.com/ervandew/supertab.git
 git clone --depth=1 https://github.com/vim-syntastic/syntastic.git
 git clone https://github.com/vim-airline/vim-airline.git
+git clone https://github.com/vim-airline/vim-airline-themes.git
 # vim-fugitive specific commands
 git clone git://github.com/tpope/vim-fugitive.git
 vim -u NONE -c "helptags vim-fugitive/doc" -c q
